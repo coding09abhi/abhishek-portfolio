87d49c6942177ebd5f5698d3c5087886f50a395c
coding09abhi <codingchannel09@gmail.com>
2025-07-27 12:11:21 +0530

    Create Home.tsx

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import { Navigation } from "@/components/Navigation";
+import { Hero } from "@/components/Hero";
+import { About } from "@/components/About";
+import { Skills } from "@/components/Skills";
+import { Projects } from "@/components/Projects";
+import { Services } from "@/components/Services";
+import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
